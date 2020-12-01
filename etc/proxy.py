bf6c0e039ae0773e1bb4ad7995d9ff6fb8abc7a2
nokyaselpon <75005487+nokyaselpon@users.noreply.github.com>
2020-12-01 14:55:07 +0800

    Update proxy.py

--- etc/proxy.py
+++ etc/proxy.py
@@ -5,18 +5,18 @@ import socket, threading, thread, select, signal, sys, time
 from os import system
 system("clear")
 #conexao
-IP = '0.0.0.0'
+IP = '139.162.65.62'
 try:
    PORT = int(sys.argv[1])
 except:
-   PORT = 80
+   PORT = 8500
 PASS = ''
 BUFLEN = 8196 * 8
 TIMEOUT = 60
 MSG = 'SSHPLUS'
 COR = '<font color="null">'
 FTAG = '</font>'
-DEFAULT_HOST = '0.0.0.0:22'
+DEFAULT_HOST = '139.162.65.62:22'
 RESPONSE = "HTTP/1.1 200 " + str(COR) + str(MSG) + str(FTAG) + "\r\n\r\n"
  
 class Server(threading.Thread):
